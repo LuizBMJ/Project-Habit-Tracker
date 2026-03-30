8d05055e0485abfb2d6b8d8bb31bfd13b8f23080
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-03-30 16:04:54 -0400

    Components Header and Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4 text-center">
+    <p>
+        Criado por Jorge Luiz. O código fonte está no <a href="https://github.com/LuizBMJ" class="underline" >GitHub</a>
+    </p>
+</footer>
