883d42d33f099f930649100c63b0975741574484
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-03-31 13:53:58 -0400

    Add login functionality and user session

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/', [App\Http\Controllers\SiteController::class, 'index']);
+// SITE
+Route::get('/', [SiteController::class, 'index']);
+
+// LOGIN
+Route::get('/login', [LoginController::class, 'index']);
