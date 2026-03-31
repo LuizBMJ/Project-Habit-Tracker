84f8ae2e99fb46fbffa67ffc166c6e208c423ca8
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-03-31 18:18:45 -0400

    Use LoginRequest and improve login view

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,12 +15,9 @@ class LoginController extends Controller
         return view('login');
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required']
-        ]);
+        $credentials = $request->only('email', 'password');
 
         if(Auth::attempt($credentials)) {
             $request->session()->regenerate();
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,30 +1,58 @@
 <x-layout>
     <main class="py-10" >
-        <h1>
-            Olá
-        </h1>
 
-        <section class="mt-4" >
-            <form action="/login" method="POST">
+        <section class="bg-white max-w-[600px] mx-auto p-10 border-2 mt-4" >
+
+            <h1 class="font-bold text-3xl">
+                Faça Login
+            </h1>
+
+            <p>
+                Insira seus dados para acessar.
+            </p>
+
+            <form action="/login" method="POST" class="flex flex-col">
                 @csrf
 
-                @error('email')
-                    <p class="text-red-500 text-xs mt-1"> {{ $message }}</p>
-                @enderror
+                <div class="flex flex-col gap-2 mb-4">
+                    <label for="email">
+                        Email
+                    </label>
 
-                <input 
-                type="email" 
-                name="email"    
-                placeholder="your@email.com"
-                class="bg-white p-2 border-2"
-                >
+                    <input 
+                    type="email" 
+                    name="email"    
+                    placeholder="your@email.com"
+                    class="bg-white p-2 border-2 @error('email') border-red-500 @enderror"
+                    >
 
-                <input 
-                type="password" 
-                name="password"    
-                placeholder="password"
-                class="bg-white p-2 border-2"
-                >
+                    <p class="text-red-500 text-sm">
+                        @error('email') 
+                            {{ $message }}
+                        @enderror
+                    </p>
+                </div>
+
+                <div class="flex flex-col gap-2 mb-4">
+
+                    <label for="password">
+                        Password
+                    </label>
+
+                    <input 
+                    type="password" 
+                    name="password"    
+                    placeholder="password"
+                    class="bg-white p-2 border-2 @error('password') border-red-500 @enderror"
+                    >
+
+                    <p class="text-red-500 text-sm">
+                        @error('password') 
+                            {{ $message }}
+                        @enderror
+                    </p>
+                </div>
+                
                 
 
                 <button 
