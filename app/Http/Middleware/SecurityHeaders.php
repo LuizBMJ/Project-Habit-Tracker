3e89e9de1219b624fe54a177258ae4900d2143fe
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-04-15 15:38:05 -0400

    Trying to fix error 500

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -21,15 +21,10 @@ class SecurityHeaders
         if (app()->environment('production')) {
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
 
-            $response->headers->set('Content-Security-Policy', "
-                default-src 'self';
-                script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-                style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https:;
-                font-src 'self' data: https://fonts.gstatic.com https:;
-                img-src 'self' data: https:;
-                connect-src 'self' https://accounts.google.com https://oauth2.googleapis.com https:;
-                frame-src https://accounts.google.com;
-            ");
+            $response->headers->set(
+                'Content-Security-Policy',
+                "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https:; font-src 'self' data: https://fonts.gstatic.com https:; img-src 'self' data: https:; connect-src 'self' https://accounts.google.com https://oauth2.googleapis.com https:; frame-src https://accounts.google.com;"
+            );
         }
 
         return $response;
