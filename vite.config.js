c23221e5d27f16dfd110bcc7cd4b6ff56d96fae4
Jorge Luiz Bevilacqua Mercer <luiz.bevilacquam@gmail.com>
2026-05-28 17:52:35 -0400

    Bug Fix: Docker remastered to work to run with localhost

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
         port: 5173,
         strictPort: true,
         cors: true,
-        origin: 'http://0.0.0.0:5173',
+        origin: 'http://localhost:5173',
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
